6356884a8b0c3377763088777fa77f6ca76329b2
Official-JA <anttonenjoel@outlook.com>
2026-02-02 12:18:51 +0200

    02/02
    Lecture

---
+++ Assets/Scripts/SimpleScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace AA0000
+{ 
+    public class SimpleScript : MonoBehaviour
+    {
+         
+        
+
+        
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
