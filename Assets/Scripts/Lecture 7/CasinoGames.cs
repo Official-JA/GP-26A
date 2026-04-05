1294ebcd9ee631187d5e43997d8f2b8dfd826e15
Official-JA <anttonenjoel@outlook.com>
2026-04-05 23:53:54 +0300

    05/04
    Module stuff

--- Assets/Scripts/Lecture 7/CasinoGames.cs
+++ Assets/Scripts/Lecture 7/CasinoGames.cs
@@ -4,40 +4,35 @@ namespace AH3520
 {
     public class CasinoGames : MonoBehaviour
     {
-        [SerializeField] public float chipAmount = 100f;
+        [SerializeField] public float chipAmount = 100f; 
         [SerializeField] public float betAmount = 10f;
         [SerializeField] private KeyCode increaseBet = KeyCode.UpArrow;
         [SerializeField] private KeyCode decreaseBet = KeyCode.DownArrow;
 
         private string chipText, betText;
 
-        void Start()
-        {
-            
-        }
-
         void Update()
         {
-            if (chipAmount > 0)
+            if (chipAmount > 0) // Can't bet with 0 chips
             {
-                if (Input.GetKeyDown(increaseBet) && betAmount < chipAmount)
+                if (Input.GetKeyDown(increaseBet) && betAmount < chipAmount) // Increasing the bet, can't bet more than you own
                 {
                     betAmount += 10;
                 }
 
-                if (Input.GetKeyDown(decreaseBet) && 10 < betAmount)
+                if (Input.GetKeyDown(decreaseBet) && 10 < betAmount) // Decreasing the bet, min. amount to bet is 10
                 {
                     betAmount -= 10;
                 }
             }
 
-            if (chipAmount == 0)
+            if (chipAmount == 0 || chipAmount < betAmount) // Fixes contradictions
             {
-                betAmount = 0;
+                betAmount = chipAmount;
             }
         }
 
-        private void OnGUI()
+        private void OnGUI() // UI stuff goes here, updates last and once per frame
         {
             GUIStyle headStyle = new GUIStyle(GUI.skin.label);
             headStyle.fontSize = 70;
--- Assets/Scripts/Lecture 7/ChipSystem.cs
+++ Assets/Scripts/Lecture 7/ChipSystem.cs
@@ -4,31 +4,6 @@ namespace AH3520
 {
     public class ChipSystem : MonoBehaviour
     {
-        [SerializeField] private int chipAmount = 100;
         
-        private string chipText;
-
-        void Start()
-        {
-
-        }
-
-        
-        void Update()
-        {
-
-        }
-
-        private void OnGUI()
-        {
-            GUIStyle headStyle = new GUIStyle(GUI.skin.label);
-            headStyle.fontSize = 70;
-            Font myFont = (Font)Resources.Load("Fonts/Louvre", typeof(Font));
-            headStyle.font = myFont;
-
-            chipText = "Chip Amount : " + chipAmount;
-
-            GUI.Label(new Rect(1800, 1300, 2000, 2000), chipText, headStyle);
-        }
     }
 }
