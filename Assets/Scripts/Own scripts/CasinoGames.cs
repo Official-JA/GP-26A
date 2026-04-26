95d5da3ef811c00ae373eb4d1d0c5225455f78b0
Official-JA <anttonenjoel@outlook.com>
2026-04-26 21:12:39 +0300

    26/04
    Stuff

---
+++ Assets/Scripts/Own scripts/CasinoGames.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace AH3520
+{
+    public class CasinoGames : MonoBehaviour
+    {
+        [SerializeField] public float chipAmount = 100f; 
+        [SerializeField] public float betAmount = 10f;
+        [SerializeField] private KeyCode increaseBet = KeyCode.UpArrow;
+        [SerializeField] private KeyCode decreaseBet = KeyCode.DownArrow;
+
+        private string chipText, betText;
+
+        void Update()
+        {
+            if (chipAmount > 0) // Can't bet with 0 chips
+            {
+                if (Input.GetKeyDown(increaseBet) && betAmount < chipAmount && betAmount < 50) // Increasing the bet, can't bet more than you own, bet limit is 50
+                {
+                    betAmount += 5;
+                }
+
+                if (Input.GetKeyDown(decreaseBet) && 5 < betAmount) // Decreasing the bet, min. amount to bet is 10
+                {
+                    betAmount -= 5;
+                }
+            }
+
+            if (chipAmount == 0 || chipAmount < betAmount) // Fixes contradictions (if any)
+            {
+                betAmount = chipAmount;
+            }
+        }
+
+        private void OnGUI() // UI stuff goes here, updates last and once per frame
+        {
+            GUIStyle headStyle = new GUIStyle(GUI.skin.label);
+            headStyle.fontSize = 70;
+            Font myFont = (Font)Resources.Load("Fonts/Louvre", typeof(Font));
+            headStyle.font = myFont;
+
+            chipText = "Chip Amount : " + chipAmount;
+            betText = "Bet Amount : " + betAmount;
+
+            GUI.Label(new Rect(1800, 1300, 2000, 2000), chipText, headStyle);            
+            GUI.Label(new Rect(1800, 1200, 2000, 2000), betText, headStyle);
+        }
+    }
+}
---
+++ Assets/Scripts/Own scripts/Cutscene.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+namespace AH3520
+{
+    public class Cutscene : MonoBehaviour
+    {
+        [SerializeField] GameObject playerPrefab;
+        [SerializeField] GameObject cameraPrefab;
+        
+        private string title = "";
+
+        IEnumerator DelayTime(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            title = "gambling.";
+        }
+
+        IEnumerator ActivatePlayer(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            playerPrefab.SetActive(true);
+            cameraPrefab.SetActive(false);
+        }
+
+        // Start 
+        void Start()
+        {
+            StartCoroutine(DelayTime(2f));
+
+            StartCoroutine(ActivatePlayer(5f));
+        }
+
+        private void OnGUI()
+        {
+            GUIStyle headStyle = new GUIStyle(GUI.skin.label);
+            headStyle.fontSize = 300;
+            Font myFont = (Font)Resources.Load("Fonts/Oregon", typeof(Font));
+            headStyle.font = myFont;
+
+            GUI.Label(new Rect(750, 600, 2000, 2000), title, headStyle);
+        }
+    }
+}
